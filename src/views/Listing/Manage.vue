918497c7c0727c41f34f22650607b3d22e989225
LvlAndFarm <walid.openrealms@gmail.com>
2020-07-09 22:22:00 +0100

    Create Manage.vue

---
+++ src/views/Listing/Manage.vue
@@ -0,0 +1,21 @@
+<template>
+    <content-page>
+        <template v-slot:main>
+            Manage user
+        </template>
+    </content-page>
+</template>
+
+<script>
+    import ContentPage from "../../components/common/pages/ContentPage";
+
+
+    export default {
+        components: {ContentPage},
+        name: "ManageListing"
+    }
+</script>
+
+<style scoped>
+
+</style>
